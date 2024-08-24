2904f18c98f8e3b11a70ba3a0f9dda3d6601df9c
verma494 <aayushverma042004@gamil.com>
2024-08-24 15:27:15 +0530

    dashboard work

--- models/UserModel.js
+++ models/UserModel.js
@@ -12,22 +12,37 @@ mongoose.connect(dbURI, {
 .catch(err => console.log('MongoDB connection error:', err));
 
 // Define the user schema
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
-  // Add more fields as necessary
+
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    fathersName: String,
+    dob: String,
+    phoneNumber: String,
+    address: String,
+    collegeName: String,
+    course: String,
+    collegeId: String,
+    casteCertificateNumber: String,
+    aadhaarNumber: String,
+    incomeCertificateNumber: String,
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
 });
 
-const User = mongoose.model('User', UserSchema);
+// Method to mask Aadhaar number
+userSchema.methods.maskAadhaar = function() {
+    return this.aadhaarNumber.replace(/\d{4}$/, 'xxxx');
+};
+
+const User = mongoose.model('User', userSchema);
+
 module.exports = User;
+
--- routes/user.js
+++ routes/user.js
@@ -13,35 +13,36 @@ router.get('/register', (req, res) => res.render('register'));
 
 // Register User
 router.post('/register', async (req, res) => {
-  const { name, email, password } = req.body;
-
-  try {
-    let user = await User.findOne({ email: email });
-    if (user) {
-      res.send('Email is already registered');
-    } else {
-      const newUser = new User({ name, email, password });
-
-      // Hash Password
-      bcrypt.genSalt(10, async (err, salt) => {
-        if (err) throw err;
-        const hash = await bcrypt.hash(newUser.password, salt);
-        newUser.password = hash;
-        await newUser.save();
-
-        // Create JWT Token
-        const token = jwt.sign({ id: newUser._id }, JWT_SECRET, { expiresIn: '1h' });
-
-        // Set JWT Token as Cookie
-        res.cookie('token', token, { httpOnly: true });
-        
-        res.redirect('/user/dashboard'); // Redirect to dashboard
-      });
+    const { name, fatherName, dob, phoneNumber, address, collegeName, course, collegeId, casteCertificateNumber, aadhaarNumber, incomeCertificateNumber, email, password } = req.body;
+  
+    try {
+      let user = await User.findOne({ email: email });
+      if (user) {
+        res.send('Email is already registered');
+      } else {
+        const newUser = new User({ name, fatherName, dob, phoneNumber, address, collegeName, course, collegeId, casteCertificateNumber, aadhaarNumber, incomeCertificateNumber, email, password });
+  
+        // Hash Password
+        bcrypt.genSalt(10, async (err, salt) => {
+          if (err) throw err;
+          const hash = await bcrypt.hash(newUser.password, salt);
+          newUser.password = hash;
+          await newUser.save();
+  
+          // Create JWT Token
+          const token = jwt.sign({ id: newUser._id }, JWT_SECRET, { expiresIn: '1h' });
+  
+          // Set JWT Token as Cookie
+          res.cookie('token', token, { httpOnly: true });
+          
+          res.redirect('/user/dashboard'); // Redirect to dashboard
+        });
+      }
+    } catch (error) {
+      res.status(500).send('Error registering user');
     }
-  } catch (error) {
-    res.status(500).send('Error registering user');
-  }
-});
+  });
+  
 
 // Login Page
 router.get('/login', (req, res) => res.render('login'));
@@ -79,24 +80,28 @@ router.get('/logout', (req, res) => {
 
 // Dashboard Page (Protected)
 router.get('/dashboard', async (req, res) => {
-  const token = req.cookies.token;
-
-  if (!token) {
-    return res.redirect('/user/login');
-  }
-
-  try {
-    const decoded = jwt.verify(token, JWT_SECRET);
-    const user = await User.findById(decoded.id);
-
-    if (!user) {
+    const token = req.cookies.token;
+  
+    if (!token) {
       return res.redirect('/user/login');
     }
-    
-    res.render('dashboard', { user });
-  } catch (err) {
-    res.redirect('/user/login');
-  }
-});
+  
+    try {
+      const decoded = jwt.verify(token, JWT_SECRET);
+      const user = await User.findById(decoded.id);
+  
+      if (!user) {
+        return res.redirect('/user/login');
+      }
+  
+      // Mask Aadhaar Number
+      const maskedAadhaarNumber = user.aadhaarNumber.replace(/(\d{4})\d{4}(\d{4})/, 'xxxx78xxx');
+  
+      res.render('dashboard', { user: { ...user._doc, aadhaarNumber: maskedAadhaarNumber } });
+    } catch (err) {
+      res.redirect('/user/login');
+    }
+  });
+  
 
 module.exports = router;
